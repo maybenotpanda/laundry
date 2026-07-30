edb7372e17fb5dcd1da65da9d891051479d4bbf6
maybenotpanda <maybenotpanda@gmail.com>
2026-07-30 10:42:05 +0700

    feat-transaction: add form script duplicat for add transaction

--- app/Views/layouts/app.php
+++ app/Views/layouts/app.php
@@ -76,6 +76,7 @@
     <script src="<?= base_url(); ?>/plugins/datatables-buttons/js/buttons.html5.min.js"></script>
     <script src="<?= base_url(); ?>/plugins/datatables-buttons/js/buttons.print.min.js"></script>
     <script src="<?= base_url(); ?>/plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
+    <?= $this->renderSection('javascript'); ?>
     <script>
         $(function() {
             $('.select2').select2()
@@ -97,6 +98,79 @@
             });
         });
 
+        document.addEventListener('DOMContentLoaded', function() {
+            setTimeout(function() {
+                $('.select2').select2();
+            }, 100);
+
+
+            const addButton = document.getElementById('addOtherServices');
+            const container = document.getElementById('serviceContainer');
+
+            // CEK HALAMAN
+            if (addButton && container) {
+                addButton.addEventListener('click', function() {
+                    let firstForm = container.querySelector('.service-item');
+                    let cloned = firstForm.cloneNode(true);
+
+                    // RESET INPUT
+                    cloned.querySelectorAll('input')
+                        .forEach(function(input) {
+                            input.value = "";
+                        });
+
+                    cloned.querySelectorAll('textarea')
+                        .forEach(function(textarea) {
+                            textarea.value = "";
+                        });
+                    cloned.querySelectorAll('select')
+                        .forEach(function(select) {
+                            select.selectedIndex = 0;
+                        });
+
+                    // UPDATE NOMOR LAYANAN
+                    let total = container.querySelectorAll('.service-item').length + 1;
+                    cloned.querySelector('.card-title').innerHTML = "Layanan #" + total;
+
+                    // TAMBAHKAN FORM BARU
+                    container.appendChild(cloned);
+
+                    // AKTIFKAN SELECT2 ULANG
+                    $(cloned).find('.select2').select2();
+                    updateRemoveButton();
+                });
+            }
+
+            // EVENT HAPUS
+            document.addEventListener('click', function(e) {
+                if (e.target.closest('.removeRow')) {
+                    let button = e.target.closest('.removeRow');
+                    let form = button.closest('.service-item');
+                    let total = container.querySelectorAll('.service-item').length;
+
+                    if (total > 1) {
+                        form.remove();
+                        updateRemoveButton();
+                    }
+                }
+            });
+        });
+
+        // ==============================
+        // ATUR TOMBOL HAPUS
+        // ==============================
+        function updateRemoveButton() {
+            let forms = document.querySelectorAll('.service-item');
+            let buttons = document.querySelectorAll('.removeRow');
+
+            buttons.forEach(function(btn) {
+                btn.style.display = "block";
+            });
+
+            if (forms.length == 1) {
+                buttons[0].style.display = "none";
+            }
+        }
     </script>
 </body>
 
